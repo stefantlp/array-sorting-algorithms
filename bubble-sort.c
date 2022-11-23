e1983094803f17533445b201cf987150ce2517ec
Stefan Talpos <stefan.talpos@stud.acs.upb.ro>
2022-11-23 09:41:08 +0200

    Add bubble sort algorithm skeleton

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,28 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-void sortare_bubble(int v[], unsigned int n);
+
+static void sort(void)
 {
-	int i,ok=1;
-	while(ok!=1)
-	{
-		ok=0;
-		for(i=0;i<n;i++)
-			if(v[i]>v[i+1])
-			{
-				ok=1;
-				v[i]=v[i]+v[i+1];
-				v[i+1]=v[i]-v[i+1];
-				v[i]=v[i]-v[i+1];
-			}
-	}
+	// TODO: add bubble sort algorithm here
 }
+
 int main()
 {
-	int v[100][100],i;
-	unsigned int n;
-	scanf("%u",&n);
-	for(i=0;i<n;i++)
-		scanf("%d",&v[i]);
-	sortare_bubble(v,n);
 	return 0;
 }
